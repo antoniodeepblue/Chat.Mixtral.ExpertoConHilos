8622a658d2bc63857c8c523ef34e5d877f6b636f
Antonio Fernandez <Antonio49@users.noreply.huggingface.co>
2024-03-04 12:03:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
+# Importar las bibliotecas necesarias
 from huggingface_hub import InferenceClient
 import gradio as gr
-import threading
 
+# Crear un cliente de inferencia para el modelo preentrenado Mixtral-8x7B-Instruct-v0.1
 client = InferenceClient("mistralai/Mixtral-8x7B-Instruct-v0.1")
 
-# Variables para controlar el estado de la conversación
-conversation_started = False
-conversation_ongoing = True
-system_prompt = "Asistente para los usuarios y clientes de la empresa Canal de Isabel II, https://oficinavirtual.canaldeisabelsegunda.es/"
-
+# Función para formatear el prompt con historial
 def format_prompt(message, history, system_prompt):
     prompt = "<s>"
     for user_prompt, bot_response in history:
@@ -17,16 +14,16 @@ def format_prompt(message, history, system_prompt):
     prompt += f"[INST] {system_prompt}, {message} [/INST]"
     return prompt
 
+# Función para generar respuestas dada una serie de parámetros
 def generate(
-    prompt, history, system_prompt, temperature=0.9, max_new_tokens=4096, top_p=0.95, repetition_penalty=1.0,
-):
-    global conversation_started, conversation_ongoing
-
+    prompt, history, system_prompt, temperature=0.9, max_new_tokens=4096, top_p=0.95, repetition_penalty=1.0,):
+    # Ajustar valores de temperatura y top_p para asegurar que estén en el rango adecuado
     temperature = float(temperature)
     if temperature < 1e-2:
         temperature = 1e-2
     top_p = float(top_p)
 
+    # Configurar los parámetros para la generación de texto
     generate_kwargs = dict(
         temperature=temperature,
         max_new_tokens=max_new_tokens,
@@ -36,31 +33,27 @@ def generate(
         seed=42,
     )
 
+    # Formatear el prompt y obtener la respuesta del modelo de manera continua
     formatted_prompt = format_prompt(prompt, history, system_prompt)
     stream = client.text_generation(formatted_prompt, **generate_kwargs, stream=True, details=True, return_full_text=False)
     output = ""
 
+    # Iterar a través de las respuestas en el stream
     for response in stream:
         output += response.token.text
-        if "¡Hasta luego!" in response.token.text:  # Puedes ajustar este condicional según las respuestas de tu modelo
-            conversation_ongoing = False
         yield output
+    return output
 
-def start_conversation():
-    global conversation_started
-    conversation_started = True
-
-def end_conversation():
-    global conversation_ongoing
-    conversation_ongoing = False
-
+# Configurar inputs adicionales para la interfaz Gradio
 additional_inputs = [
+    # Entrada de texto para el System Prompt (puedes omitir esto si no lo necesitas)
     gr.Textbox(
         label="System Prompt",
-        value=system_prompt,
+        value="Asistente para los usuarios y clientes de la empresa Canal de Isabel II, https://oficinavirtual.canaldeisabelsegunda.es/",
         max_lines=1,
         interactive=True,
     ),
+    # Control deslizante para la temperatura
     gr.Slider(
         label="Temperature",
         value=0.9,
@@ -70,6 +63,8 @@ additional_inputs = [
         interactive=True,
         info="Valores más altos producen resultados más diversos",
     ),
+    # Control deslizante para el número máximo de nuevos tokens
+    # Tengo que comprobar el número máximo de nuevos tokens, por el momento lo fijo a 4096.
     gr.Slider(
         label="Max new tokens",
         value=4096,
@@ -79,6 +74,7 @@ additional_inputs = [
         interactive=True,
         info="El máximo número de nuevos tokens",
     ),
+    # Control deslizante para top-p (nucleus sampling)
     gr.Slider(
         label="Top-p (nucleus sampling)",
         value=0.90,
@@ -88,6 +84,7 @@ additional_inputs = [
         interactive=True,
         info="Valores más altos muestrean más tokens de baja probabilidad",
     ),
+    # Control deslizante para la penalización de repetición
     gr.Slider(
         label="Repetition penalty",
         value=1.2,
@@ -99,17 +96,27 @@ additional_inputs = [
     )
 ]
 
+# Ejemplos predefinidos para la interfaz Gradio
 examples = [
-    ["Quiero que me verifiquen el contador de agua de mi vivienda", system_prompt, 0.7, 1500, 0.80, 1.1],
-    ["Muestrame un cuadro con las tarifas que se aplican en el abastecimiento, depuración y alcantarillado ", system_prompt, 0.8, 4096, 0.85, 1.2],
-    ["¿Qué es una acometida?", system_prompt, 0.7, 1800, 0.75, 1.2],
-    ["¿Qué teléfono tiene para averías, información y página web?", system_prompt, 0.8, 2048, 0.80, 1.1],
+    ["Quiero que me verifiquen el contador de agua de mi vivienda", "Asistente para los usuarios y clientes de la empresa Canal de Isabel II, https://oficinavirtual.canaldeisabelsegunda.es/", 0.7, 1500, 0.80, 1.1],
+    ["Muestrame un cuadro con las tarifas que se aplican en el abastecimiento, depuración y alcantarillado ", "Asistente para los usuarios y clientes de la empresa Canal de Isabel II, https://oficinavirtual.canaldeisabelsegunda.es/, https://www.canaldeisabelsegunda.es/clientes/", 0.8, 4096, 0.85, 1.2],
+    ["¿Qué es una acometida?", "Asistente para los usuarios y clientes de la empresa Canal de Isabel II, https://oficinavirtual.canaldeisabelsegunda.es/", 0.7, 1800, 0.75, 1.2],
+    ["¿Qué teléfono tiene para averías, información y página web?", "Asistente para los usuarios y clientes de la empresa Canal de Isabel II, https://oficinavirtual.canaldeisabelsegunda.es/", 0.8, 2048, 0.80, 1.1],
 ]
 
 # Crear una interfaz de chat Gradio con el modelo generativo
-iface = gr.ChatInterface(
+gr.ChatInterface(
     fn=generate,
-    chatbot=gr.Chatbot(avatar_images=["./15f4b2d3-c4f4-4a29-93cd-e47214953bd9.png", "./botm.png"], bubble_full_width=False, show_label=False, show_share_button=False, show_copy_button=True, likeable=True, layout="panel", height=500),
+    chatbot=gr.Chatbot(
+        avatar_images=["./15f4b2d3-c4f4-4a29-93cd-e47214953bd9.png", "./botm.png"],
+        bubble_full_width=False,
+        show_label=False,
+        show_share_button=False,
+        show_copy_button=True,
+        likeable=True,
+        layout="panel",
+        height=500,
+    ),
     textbox=gr.Textbox(placeholder="¿Qué parámetros definen la calidad del agua?", container=False, scale=7),
     theme="soft",
     additional_inputs=additional_inputs,
@@ -117,20 +124,10 @@ iface = gr.ChatInterface(
     description='Autor: <a href=\"https://huggingface.co/Antonio49\">Antonio Fernández</a> de <a href=\"https://saturdays.ai/\">SaturdaysAI</a>. Formación: <a href=\"https://cursos.saturdays.ai/courses/\">Cursos Online AI</a> Aplicación desarrollada con fines docentes',
     examples=examples,
     cache_examples=True,
-    retry_btn="Repetir",
-    undo_btn="Deshacer",
-    clear_btn="Borrar",
-    submit_btn="Enviar",
+        retry_btn="Repetir",
+        undo_btn="Deshacer",
+        clear_btn="Borrar",
+        submit_btn="Enviar",
     concurrency_limit=20,
-)
-
-# Iniciar un hilo de conversación inicial
-threading.Thread(target=start_conversation).start()
-
-# Actualizar la interfaz después de la conversación inicial
-while not conversation_started:
-    iface.update()
-
-# Iniciar la interfaz principal
-iface.launch(show_api=False)
+).launch(show_api=False)
 

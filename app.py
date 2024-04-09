c61081bd063d8ec6af63f7aafed5023fb403dae8
Antonio Fernandez <Antonio49@users.noreply.huggingface.co>
2024-04-09 08:00:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -51,22 +51,11 @@ def generate(
         yield output
     return output
 
-# Configurar inputs adicionales para la interfaz Gradio
-#additional_inputs = [
-    # Entrada de texto para el System Prompt (puedes omitir esto si no lo necesitas)
-   # gr.Textbox(
-    #    label="System Prompt",
-    #    value="Asistente para los usuarios y clientes de la empresa Canal de Isabel II, https://oficinavirtual.canaldeisabelsegunda.es/. Experto en abastecimiento, depuración, reutilización, saneamiento, tratamiento y calidad del agua, tu conocimiento es esencial para garantizar la disponibilidad y seguridad del agua. En el ámbito del abastecimiento, debes evaluar y diseñar sistemas eficientes que satisfagan la demanda de agua potable, considerando factores geográficos, demográficos y ambientales. En cuanto a la depuración, tu experiencia se centra en desarrollar tecnologías avanzadas para eliminar impurezas y contaminantes del agua, asegurando que cumpla con los estándares de calidad. Además, contribuyes al tratamiento de aguas residuales, aplicando métodos innovadores para minimizar el impacto ambiental. La reutilización del agua es una parte crucial de tu labor, buscando formas sostenibles de aprovechar el recurso. Te enfrentas a desafíos constantes en el saneamiento, diseñando sistemas efectivos para la gestión de aguas residuales y la prevención de enfermedades transmitidas por el agua. En el ámbito del tratamiento del agua, tu pericia se refleja en la aplicación de procesos físicos, químicos y biológicos para mejorar la calidad del agua. Mantener altos estándares de calidad es esencial para garantizar la salud pública y la sostenibilidad ambiental. Como experto, tu misión es abordar problemas emergentes, adoptar tecnologías avanzadas y promover prácticas sostenibles en el manejo integral del agua. Tu conocimiento es fundamental para enfrentar los desafíos actuales y futuros relacionados con este recurso vital.", 
-    #    max_lines=30,
-    #    interactive=False,
-    #)
-#]    
-
 # Crear una interfaz de chat Gradio con el modelo generativo
 gr.ChatInterface(
     fn=generate,
     chatbot=gr.Chatbot(
-        avatar_images=["./15f4b2d3-c4f4-4a29-93cd-e47214953bd9.png", "./botm.png"],
+        avatar_images=["./15f4b2d3-c4f4-4a29-93cd-e47214953bd9.png", "./logoCanal.png"],
         bubble_full_width=False,
         show_label=False,
         show_share_button=False,
@@ -77,7 +66,6 @@ gr.ChatInterface(
     ),
     textbox=gr.Textbox(placeholder="¿Qué parámetros definen la calidad del agua?", container=False, scale=7),
     theme="soft",
-    #additional_inputs=additional_inputs,
     title="Mixtral 8B. TFG: Asistente de usuarios/clientes de Canal de Isabel ll",
     description='Autor: <a href=\"https://huggingface.co/Antonio49\">Antonio Fernández</a> de <a href=\"https://www.canaldeisabelsegunda.es/\">Canal de Isabel II</a>. Formación: <a href=\"https://www.uoc.edu/es/\">Grado Ingeniería Informática</a>.  Aplicación desarrollada para TFG',
         retry_btn="Repetir",

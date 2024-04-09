a90e46c2afa096335441f286269df2573c02efe1
Antonio Fernandez <Antonio49@users.noreply.huggingface.co>
2024-04-09 08:28:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def format_prompt(message, history, system_prompt):
 def generate(
     prompt, 
     history, 
-    system_prompt= "Asistente para los usuarios y clientes de la empresa Canal de Isabel II, https://oficinavirtual.canaldeisabelsegunda.es/ ", 
+    system_prompt= "Experto en servicios de abastecimiento, depuración, reutilización, alcantarillado y calidad del agua, para la empresa Canal de Isabel II.  Asistente que ofrece información: obras hidráulicas, cumplimiento de sanidad y seguridad de las instalaciones del Canal de Isabel II", 
     temperature=0.9, 
     max_new_tokens=4096, 
     top_p=0.95, 
@@ -55,7 +55,7 @@ def generate(
 gr.ChatInterface(
     fn=generate,
     chatbot=gr.Chatbot(
-        avatar_images=["./15f4b2d3-c4f4-4a29-93cd-e47214953bd9.png", "./logoCanal.png"],
+        avatar_images=["./botm.png", "./logoCanal.png"],
         bubble_full_width=False,
         show_label=False,
         show_share_button=False,
@@ -64,9 +64,9 @@ gr.ChatInterface(
         layout="panel",
         height=500,
     ),
-    textbox=gr.Textbox(placeholder="¿Qué parámetros definen la calidad del agua?", container=False, scale=7),
+    textbox=gr.Textbox(placeholder="¿Qué es una tubería de polietiteno?", container=False, scale=7),
     theme="soft",
-    title="Mixtral 8B. TFG: Asistente de usuarios/clientes de Canal de Isabel ll",
+    title="Mixtral 8B. TFG: Experto en sector del agua del Canal de Isabel ll",
     description='Autor: <a href=\"https://huggingface.co/Antonio49\">Antonio Fernández</a> de <a href=\"https://www.canaldeisabelsegunda.es/\">Canal de Isabel II</a>. Formación: <a href=\"https://www.uoc.edu/es/\">Grado Ingeniería Informática</a>.  Aplicación desarrollada para TFG',
         retry_btn="Repetir",
         undo_btn="Deshacer",
